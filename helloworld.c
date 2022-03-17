3f2011e75d24f8a79a79d7ef9dda9d9a5cc64e6a
Danilo <chinendanilo@gmail.com>
2022-03-17 12:36:23 -0300

    Created list of random numbers

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 10
 int main()
 {
-    printf("Hello, World!");
-    printf("\nroblox");
-    printf("mudei diferente");
+    srand(time(NULL));
+    int i;
+    int *lista;
+    lista = (int*) malloc (sizeof(int)*N);
+    printf("lista: ");
+    for (i=0; i<N; i++)
+    {
+        lista[i] = rand();
+        printf("%d ", lista[i]);
+    }
     return 0;
 }
